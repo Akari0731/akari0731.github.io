212a6ea245725afff1cf3f1282446505e5cdcae4
Akari Nishii <nishii0731@gmail.com>
2022-10-30 10:49:17 +0100

    Add profile photo

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -2,11 +2,12 @@ import { Page } from 'components/Main/types';
 import React from 'react';
 import './index.css';
 import { MenuItem } from './MenuItem';
+import profile from '../assets/profile.jpg';
 
 export const SideMenu: React.FC = () => {
   return (
     <div className="sideMenu">
-      <div className="icon"></div>
+      <img className="icon" src={profile} />
       <MenuItem page={Page.ABOUT} text="ABOUT" />
       <MenuItem page={Page.EXPERIENCE} text="EXPERIENCE" />
       <MenuItem page={Page.EDUCATION} text="EDUCATION" />
