86e93cdf19dad52c9da9a69034f43e8098108bd6
Akari Nishii <nishii0731@gmail.com>
2022-10-30 12:14:49 +0100

    Add personality

--- src/components/Main/PersonalityPage/index.tsx
+++ src/components/Main/PersonalityPage/index.tsx
@@ -4,10 +4,22 @@ import './index.css';
 export const PersonalityPage: React.FC = () => {
   return (
     <>
-      <h2>Personality</h2>
-      <div>
-        Full stack developer with 3+ years of experience who loves building web applications.
-      </div>
+      <h2 className="mt-0">Personality</h2>
+      <p>
+        I am 29 years old and came from Japan. I speak English fluently and I am currently learning
+        Swedish in SFI and planning to stay Gothenburg long.
+        <br />
+        <br />
+        I am very social person who likes talking, playing sports and having AWs.
+        <br />I play football, volleyball and softball. Currently joining one korpen team,{' '}
+        <a href="https://www.rampen.co/" target="_blank" rel="noreferrer" className="text-primary">
+          Rampen
+        </a>{' '}
+        and playing football. Going Fishing in summar and going snowboarding in winter too:)
+        <br />
+        <br />I really enjoy coding, and building product together with the team. It&apos;s fun to
+        learn new framework/language and tools for me, and looking forward to take new challenges!
+      </p>
     </>
   );
 };
